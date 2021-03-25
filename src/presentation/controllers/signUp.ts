87936d4173f25fd8017c63656e7b1dbba327402a
Nau Nau <nauanycc@outlook.com.br>
2021-03-25 15:18:44 -0300

    refactor: simplify code

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -4,12 +4,13 @@ import { badRequest } from "../helpers/http-helper"
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamsError("name"))
+    const requiredFields = ["name", "email"]
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamsError(field))
+      }
     }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamsError("email"))
-    }
+
     throw new Error("Shouldn't be reachable")
   }
 }
